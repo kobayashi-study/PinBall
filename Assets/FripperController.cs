263325ce2bb0bebb520bef06f0844cb360e60a3f
kobayashi-study <yamatokobayashi1225@gmail.com>
2024-12-13 20:32:37 +0900

    12/13の修正
    ①指を離した時のフリッパーの挙動についてのコードを修正しました。
    
    ②for文を使用してコードの最適化を行ないました。

--- Assets/FripperController.cs
+++ Assets/FripperController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class FripperController : MonoBehaviour
@@ -8,7 +9,7 @@ public class FripperController : MonoBehaviour
     private float defaultAngle = 20;
     private float flickAngle = -20;
 
-    private Touch touch;
+    Touch[] list = new Touch[2];
 
     // Start is called before the first frame update
     void Start()
@@ -20,89 +21,27 @@ public class FripperController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag") {
-            SetAngle(this.flickAngle);
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag") {
-            SetAngle(this.flickAngle);
-        }
-        if (Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFripperTag") {
-            SetAngle(this.defaultAngle);            
-        }
-        if (Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFripperTag") {
-            SetAngle(this.defaultAngle);
-        }
-        */
-        //lesson5の追加課題「キーボード操作」↓
-        /*
-        if (Input.GetKeyDown(KeyCode.A) && tag == "LeftFripperTag")
-        {
-            SetAngle(this.flickAngle);
-        }
-        if (Input.GetKeyDown(KeyCode.D) && tag == "RightFripperTag")
-        {
-            SetAngle(this.flickAngle);
-        }
-        if (Input.GetKeyUp(KeyCode.A) && tag == "LeftFripperTag")
-        {
-            SetAngle(this.defaultAngle);
-        }
-        if (Input.GetKeyUp(KeyCode.D) && tag == "RightFripperTag")
-        {
-            SetAngle(this.defaultAngle);
-        }
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            SetAngle(this.flickAngle);
-        }
-        if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow))
-        {
-            SetAngle(this.defaultAngle);
-        }
-        */
         //lesson6の追加課題「タッチ操作」
-        if (Input.touchCount == 0)
-        {
+        if (Input.touchCount == 0) {
             SetAngle(this.defaultAngle);
-        }
-        else if (Input.touchCount == 1)
-        {
-            this.touch = Input.GetTouch(0);
-            if (tag == "LeftFripperTag" && touch.position.x < Screen.width / 2)
-            {
-                SetAngle(this.flickAngle);
-            }
-            else if (tag == "RightFripperTag" && touch.position.x >= Screen.width / 2)
-            {
-                SetAngle(this.flickAngle);
-            }
-        }
-        else if (Input.touchCount == 2)
-        {
-            this.touch = Input.GetTouch(0);
-            if (tag == "LeftFripperTag" && touch.position.x < Screen.width / 2)
-            {
-                SetAngle(this.flickAngle);
-            }
-            else if (tag == "RightFripperTag" && touch.position.x >= Screen.width / 2)
-            {
-                SetAngle(this.flickAngle);
-            }
-            this.touch = Input.GetTouch(1);
-            if (tag == "LeftFripperTag" && touch.position.x < Screen.width / 2)
-            {
-                SetAngle(this.flickAngle);
-            }
-            else if (tag == "RightFripperTag" && touch.position.x >= Screen.width / 2)
-            {
-                SetAngle(this.flickAngle);
+        } else {
+            for (int i = 0; i < Input.touchCount; i++) {
+                list[i] = Input.GetTouch(i);
+                if (tag == "LeftFripperTag" && list[i].position.x < Screen.width / 2) {
+                    SetAngle(this.flickAngle);
+                } else if (tag == "RightFripperTag" && list[i].position.x >= Screen.width / 2) {
+                    SetAngle(this.flickAngle);
+                }
             }
         }
 
-        if (touch.phase == TouchPhase.Ended)
-        {
-            SetAngle(this.defaultAngle);
+        for (int j = 0; j < Input.touchCount; j++) {
+            if (tag == "LeftFripperTag" && list[j].position.x < Screen.width / 2 && list[j].phase == TouchPhase.Ended) {
+                SetAngle(this.defaultAngle);
+            }
+            else if (tag == "RightFripperTag" && list[j].position.x >= Screen.width / 2 && list[j].phase == TouchPhase.Ended) {
+                SetAngle(this.defaultAngle);
+            }
         }
     }
 
